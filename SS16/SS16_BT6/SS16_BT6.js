7929bfee1e3c1fc965ad475ac2ac704425cac8e3
TranQuoc2k6 <qatran2k6@gmail.com>
2025-12-29 10:07:17 +0700

    SS16

---
+++ SS16/SS16_BT6/SS16_BT6.js
@@ -0,0 +1,3 @@
+let dola = +prompt("Nhập số tiền $: ");
+let vnd = dola * 25000;
+alert(`${dola}$ = ${vnd} đ`);
---
+++ SS16/SS16_BT8/SS16_BT8.js
@@ -0,0 +1,6 @@
+const pi = 3.14;
+let r = +prompt("Nhap ban kinh hinh tron:");
+let chuViHinhTron = 2 * pi * r;
+let dienTichHinhTron = pi * r ** 2;
+console.log(`Chu vi hinh tron = ${chuViHinhTron}`);
+console.log(`Dien tich hinh tron = ${dienTichHinhTron}`);
